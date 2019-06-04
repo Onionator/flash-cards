67d073747ec17b63f7524da6c274a34fcbabda3f
jacob Irwin and Sam Cook <sam@>
2019-06-04 15:42:12 -0700

    added full functionality to flashcards. used toggle functions to display definitions

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  //click function that hides term and shows definition
+  $(".col-md-3").click(function() {
+    $(this).children("p").toggle();
+    $(this).children("h1").toggle();
+  });
+});
